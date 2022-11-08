31bf46c30314e13c1073ad40cd104061bd7697b8
taha.arshad <taha.arshad10@outlook.com>
2022-11-08 11:02:15 +0400

    week 7

---
+++ product.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Something 1",
+        "description": "Description",
+        "price": 50.00,
+        "image": "images/Petfood.jpg",
+        "availableInventory": 5,
+        "rating": 1
+    },
+    {
+        "id": 1002,
+        "title": "Something 2",
+        "description": "Description",
+        "price": 70.00,
+        "image": "images/Petfood.jpg",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id": 1003,
+        "title": "Something 3",
+        "description": "Description",
+        "price": 30.00,
+        "image": "images/Petfood.jpg",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 1004,
+        "title": "Something 4",
+        "description": "Description",
+        "price": 20.00,
+        "image": "images/Petfood.jpg",
+        "availableInventory": 5,
+        "rating": 5
+    }
+]
